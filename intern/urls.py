0911e10f66b285144a10966727fce63ce202531c
aayush-1 <ee17btech11003@iith.ac.in>
2020-09-04 23:10:37 +0530

    a

--- intern/urls.py
+++ intern/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 	path("portal/", views.portal, name="portal"),
 	path("login_student/", views.login_student, name="login_student"),
 	path("login_professor/", views.login_professor, name="login_professor"),
+	path("register_student/", views.login_student, name="register_student"),
+	path("register_professor/", views.login_professor, name="register_professor"),
 	# path("registration/", views.registration_student, name="registration"),
     path("<int:pk>/", views.prof_detail, name="prof_detail"),
 
