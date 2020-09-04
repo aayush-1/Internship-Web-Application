9c540884adbe9c21fcb2b98df7a8c311b53f8461
aayush-1 <ee17btech11003@iith.ac.in>
2020-09-04 22:23:44 +0530

    a

--- intern/urls.py
+++ intern/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
 
+	path("portal/", views.portal, name="portal"),
+	path("login_student/", views.login_student, name="login_student"),
+	path("login_professor/", views.login_professor, name="login_professor"),
+	# path("registration/", views.registration_student, name="registration"),
     path("<int:pk>/", views.prof_detail, name="prof_detail"),
 
 ]
--- intern/views.py
+++ intern/views.py
@@ -16,3 +16,14 @@ def prof_detail(request, pk):
     }
 
     return render(request, "prof_detail.html", context)
+
+
+def portal(request):
+    return render(request, "portal.html")
+
+
+def login_student(request):
+    return render(request, "login_student.html")
+
+def login_professor(request):
+    return render(request, "login_professor.html")
